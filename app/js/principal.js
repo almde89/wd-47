79550e16e7d9d00e5f6dc956879bbbc762621b98
cust6792 <cust6792@caelumBSB03-17>
2017-08-03 17:25:14 -0300

    Aula 4 Regex
    - regex
    - mais sobre o jquery

--- app/js/principal.js
+++ app/js/principal.js
@@ -18,7 +18,7 @@
         };
     }
 
-    function removerCartao () {
+    function removerCartao (_self) {
         return function (evento) {
             /* 
                 as duas maneiras existentes para acesso de um atributo criado.
@@ -26,7 +26,7 @@
             */
             //document.querySelector('#cartao'.concat(this.getAttribute('data-cartao'))).remove();
             //document.querySelector('#cartao'.concat(this.dataset.cartao)).remove();
-            var cartao = document.querySelector('#cartao'.concat(this.dataset.cartao));
+            var cartao = document.querySelector('#cartao'.concat(_self.get(0).dataset.cartao));
             cartao.classList.toggle('cartao--some');
             setTimeout(function () {cartao.remove();}, 1000);
         };
@@ -48,16 +48,18 @@
             .text('Remove')
             .attr('data-cartao', id);
         return botaoRemover
-            .click(removerCartao());
+            .click(removerCartao(botaoRemover));
     }
 
     function criarCartao (conteudoDigitado, id) {
         var novoCartao = $('<div>')
             .addClass('cartao')
+            .addClass(defineTipoCartao(conteudoDigitado))
             .attr('id', 'cartao'.concat(id));
         var conteudoNovoCartao = $('<p>')
             .addClass('cartao-conteudo')
-            .text(conteudoDigitado);
+            //.text(conteudoDigitado);/* Equivale ao textContent */
+            .html(conteudoDigitado);/* Equivale ao innerHtml */
         novoCartao.append(criarOpcoesDoCartao(id)).append(conteudoNovoCartao);
         
         return novoCartao;
@@ -67,12 +69,38 @@
         return function (evento) {
             evento.preventDefault();
             var conteudoDigitado = $('.novoCartao-conteudo', this).val()
-                .trim().replace(/\n/g, '<br>');
+                .trim().replace(/\n/g, '<br>')
+                .replace(/(\*\*)(\w+)(\*\*)/g, function (match, p1, p2, p3, offset, string) {
+                    return '<b>' + p2 + '</b>';
+                }).replace(/(\*)(\w+)(\*)/g, function (match, p1, p2, p3, offset, string) {
+                    return '<b>' + p2 + '</b>';
+                });;
 
             if (conteudoDigitado) {
                 adicionarNovoCartao(conteudoDigitado, ++numCartoes);
             }
         };
+    }
+
+    function defineTipoCartao(texto) {
+        var quebras = texto.split('<br>').length - 1;
+        var textoSemQuebras = texto.replace('<br>', ' ');
+        var letras = textoSemQuebras.length;
+        var palavras = textoSemQuebras.split(' ');
+        var tamanhoMaiorPalavra = 0;
+
+        $.each(palavras, function() {
+            var palavra = this;
+            if (palavra.length > tamanhoMaiorPalavra) tamanhoMaiorPalavra = palavra.length;
+        }, this);
+
+        if (quebras < 5 && letras < 55 && tamanhoMaiorPalavra < 9) {
+            return 'cartao--textoGrande';
+        } else if (quebras < 6 && letras < 75 && tamanhoMaiorPalavra < 12) {
+            return 'cartao--textoMedio';
+        } else {
+            return 'cartao--textoPequeno';
+        }
     }
 
     var btComutaLayout = document.querySelector('#mudaLayout');
@@ -85,4 +113,13 @@
 
     var formNovoCartao = $('.novoCartao');
     formNovoCartao.submit(salvarCartao(formNovoCartao));
+
+    $('#busca').on('input', function () {
+        var buscaDigitada = $(this).val().trim();
+        var regexBusca = new RegExp(buscaDigitada, 'i');
+        $('.cartao').hide().filter(function () {
+            return regexBusca.test($('.cartao-conteudo', this).text());/* $('.cartao-conteudo', this) para deixar de pegar
+            coisas que não são referente ao texto de visualização do cartão. Sem isso poderíamos pegar o conteúdo do botão 'remove'*/
+        }).show();
+    });
 })(jQuery);
